c301b9a27adebf121c1d0c446d44d771064e8821
priya <priya00020130@gmail.com>
2022-06-13 21:32:46 +0530

    implemented mobile navbar

--- js/common.js
+++ js/common.js
@@ -1,20 +1,37 @@
 function load_navbar_and_footer() {
     let body = document.getElementsByTagName('BODY')[0];
-    const navbar = '<nav class="desktop-nav">'+
+    const desktop_navbar = '<nav class="desktop-nav">'+
                         '<a href="index.html">HOME</a>'+
                         '<a href="about.html">ABOUT</a>'+
                         '<a href="projects.html">PROJECTS</a>'+
                         '<a>RESUME</a>'+
-                        '<a href="art.html">ART</a>'+
                     '</nav>'
     footer_text = '<footer>'+
                     '<p>Designed and Built with <i class="ri-heart-fill"></i> by <a href="#" class="pps">~pps</a></p>'+
                 '</footer>'
-    body.innerHTML = navbar + body.innerHTML +footer_text;
+    body.innerHTML = desktop_navbar + body.innerHTML +footer_text;
+}
+
+function on_menu_button_click() {
+    const menu_button = document.getElementsByClassName("mobile-nav")[0];
+    if (menu_button.classList.contains("clicked-menu-button")){
+        menu_button.classList.remove("clicked-menu-button");
+    }
+    else {
+        menu_button.classList.add("clicked-menu-button");
+    }  
+} 
+
+
+function register_menu_button_click_handler() {
+    const menu_button = document.getElementsByClassName("menu-button")[0];
+    menu_button.addEventListener("click", on_menu_button_click);
+
 }
 
 function on_page_load() {
     load_navbar_and_footer();
+    register_menu_button_click_handler();
 }
 
 window.onload = on_page_load;
